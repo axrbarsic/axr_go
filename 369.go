dc458eb5390f4b0a148c8a2f1d595a4f3f9e9ec3
axrbarsic <axrbarsic@gmail.com>
2018-06-30 23:06:28 -0400

    Update 369.go

--- 369.go
+++ 369.go
@@ -1,33 +1,25 @@
+//Напишите программу, которая выводит числа от 1 до 100, которые делятся на 3. (3, 6, 9, …).
+//Напишите программу, которая выводит числа от 1 до 100. Но для кратных трём нужно вывести «Fizz» вместо числа, для кратных пяти - «Buzz», 
+//а для кратных как трём, так и пяти — «FizzBuzz».
+
+
+
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	three := "Fizz" //переменная кратная трем
-	five := "Buzz" //переменная кратная пяти
-	i := 1
-	for i < 100 {
+	i :=1
+	for i <= 100 {
+		if i % 3 == 0 && i % 5 == 0 {  //тоже пойдет if i % 15 == 0 {
+			fmt.Println(i, "FizzBuzz", "число делится на 3 и 5")
+		} else if i % 5 == 0 {
+			fmt.Println(i,"Buzz", "число делится на 5")
+		} else  if i % 3 == 0 {
+			fmt.Println(i,"Fizz", "число делится на 3")
+			} else {
+				fmt.Println(i, "простые числа")
+		}
 		i = i + 1
-		if i % 3 == 0 { //условие кратное трем
-			i := three
-			fmt.Println(i)
-		} else {
-			fmt.Println(i)
-			if i % 5 == 0 { // условие кратное пяти
-				i := five
-			fmt.Println(i)
-		} else {
-			fmt.Println(three+five)
-		}
-		}
 	}
 }
-
-
-
-//if i % 3 == 0 {
-//fmt.Println(i, "Fizz")
-//} else {
-//fmt.Println(i, "не четное")
